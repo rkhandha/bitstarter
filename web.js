15aecb24a3065c14231dbd06a9a2e7547d24bcf8
Rekha Khandhadia <rkhandha@hotmail.com>
2013-07-07 15:41:28 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+var indexBuffer = fs.readFileSync('index.html').toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(indexBuffer);
 });
 
 var port = process.env.PORT || 5000;
